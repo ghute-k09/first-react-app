72d9cdc2d6fab6b83cf8c38b82614c7c19938a88
Kailas Ghute <kpaghute@gmail.com>
2023-06-17 23:47:31 +0530

    html+js=jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-function App () {
-  return <>
-  <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat non a dolores consequatur, laudantium ab molestias corporis temporibus quis fugit est nobis laborum animi deserunt asperiores necessitatibus perferendis debitis velit.
+function App() {
+  let str = `hello world`;
+  let pi = 3.14;
+  let active = true;
 
-  </h1>
-  </>
+  return (
+    <>
+      <h1>hello world</h1>
+      <h1>{str}</h1>
+      <h1>{str.toUpperCase()}</h1>
+      {active && <h1>good morning</h1>}
+      {!active ? <h1>good morning</h1> : <h1>good night</h1>}
+    </>
+  );
 }
 
 export default App;
