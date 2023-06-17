9d28b446289d17f05d47c2d6696bd22b2bdaa415
Kailas Ghute <kpaghute@gmail.com>
2023-06-18 00:21:35 +0530

    js with css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import "./App.css";
 function App() {
-  let str = "hello world";
-  let style = {backgroundColor:"yellow", color:"white"};
-
-  return <>
-  <h1 style={{backgroundColor: "red"}}>hello world</h1>
-  <h1 style={style}>{str}</h1>
+  return (
+  <>
+    <h1>hi</h1>
+    <h1 className="parent">hello world</h1>
   </>
+  );
 }
 
 export default App;
