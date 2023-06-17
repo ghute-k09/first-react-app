843531b6d3389a02584187312d17d03581418a2a
Kailas Ghute <kpaghute@gmail.com>
2023-06-17 18:23:58 +0530

    jsx with variables

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-function App(){
-  return <>
-             <h1>Hello World</h1>
-             <h1>Good Evening</h1>
-         </>
+function App () {
+  let jsx1 = <h1>Good Morning</h1>
+  let jsx2 = <>
+  <h1>Good Night</h1>
+  </>
+  return false ? jsx1 : jsx2;
 }
 
 export default App;
