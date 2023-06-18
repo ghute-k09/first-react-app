0b751d1bb081245f5dc2c2dbdfc6c53b73394e8d
Kailas Ghute <kpaghute@gmail.com>
2023-06-18 13:27:27 +0530

    calling another function in main fn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import "./App.css";
 function App() {
   return (
-  <>
-    <h1>hi</h1>
-    <h1 className="parent">hello world</h1>
-  </>
+    <>
+      <h1 className="bg-primary p-3 text-light">Project</h1>
+
+      <DataBlock />
+      <DataBlock />
+      <DataBlock />
+    </>
   );
 }
 
+function DataBlock() {
+  return (
+    <>
+      <div className="alert alert-warning">
+        <h1>topic 1</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tempore
+        porro dolorum sapiente autem ipsum assumenda, doloremque molestiae
+        quaerat magni doloribus eaque laboriosam. Tenetur in sit natus et
+        possimus cupiditate nam dolorem unde quia! Atque distinctio facere eius
+        magnam dolore eaque accusamus, impedit illo adipisci.
+      </div>
+    </>
+  );
+}
 export default App;
