3e7134ca4f2c7f30c7b1b143deb80f521d787387
Kailas Ghute <kpaghute@gmail.com>
2023-06-18 16:57:29 +0530

    reusability

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 function App() {
   return (
     <>
-      <h1 className="bg-primary p-3 text-light">Project</h1>
-
-      <DataBlock />
-      <DataBlock />
-      <DataBlock />
+      <AppHeader />
+      <AppBody />
+      <AppFooter />
     </>
   );
 }
 
-function DataBlock() {
+function AppHeader() {
   return (
     <>
-      <div className="alert alert-warning">
-        <h1>topic 1</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tempore
-        porro dolorum sapiente autem ipsum assumenda, doloremque molestiae
-        quaerat magni doloribus eaque laboriosam. Tenetur in sit natus et
-        possimus cupiditate nam dolorem unde quia! Atque distinctio facere eius
-        magnam dolore eaque accusamus, impedit illo adipisci.
+      <div
+        className=" row bg-primary text-light fs-1 p-3"
+        style={{ cursor: "Pointer" }}
+      >
+        <div className="col">Project</div>
+        <div className="col">Home</div>
+        <div className="col">About Us</div>
       </div>
     </>
   );
 }
+
+function AppBody() {
+  return (
+    <>
+      <div className="alert alert-success mt-2 fs-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quae,
+        architecto incidunt iure, aperiam sapiente reiciendis esse praesentium
+        dicta autem voluptates deserunt debitis nemo voluptatibus inventore
+        recusandae ullam sed, impedit in magnam. Tenetur non, totam amet
+        reiciendis nesciunt perferendis harum architecto commodi ad voluptates
+        cupiditate!Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+        quae, architecto incidunt iure, aperiam sapiente reiciendis esse
+        praesentium dicta autem voluptates deserunt debitis nemo voluptatibus
+        inventore recusandae ullam sed, impedit in magnam. Tenetur non, totam
+        amet reiciendis nesciunt perferendis harum architecto commodi ad
+        voluptates cupiditate!
+      </div>
+    </>
+  );
+}
+
+function AppFooter() {
+  return (
+    <>
+      <div
+        className=" d-flex justify-content-center align-items-center bg-secondary text-light"
+        style={{ height: 250 }}
+      >
+        Lorem ipsum dolor sit amet consectetur
+      </div>
+    </>
+  );
+}
+
 export default App;
