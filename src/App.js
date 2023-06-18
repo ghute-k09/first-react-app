c6ec1c032ce892841154fb8c32d070e3782f78fd
Kailas Ghute <kpaghute@gmail.com>
2023-06-18 17:31:37 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import AppHeader from "./pages/AppHeader";
-import AppBody from "./pages/AppBody";
-import AppFooter from "./pages/AppFooter";
+import { useState } from "react";
+
 function App() {
   return (
     <>
-      <AppHeader />
-      
-      <AppBody />
-      <AppFooter />
+      <h1>Counter App</h1>
+      <Counter />
     </>
   );
 }
 
+function Counter() {
+  //let counter = 1; stateless variable
+  let [counter, setCounter] = useState(1);//stateful variable which performs dom operations
 
-
-
-
-
+  let LikeMeAction = () => {
+    counter = counter + 1;
+    console.log("counter:"+counter);
+   //asking react to perform dom operations
+    setCounter(counter);
+    
+  }
+  return (
+    <>
+      <h1>{counter}</h1>
+      <input type="button" value="LikeMe" onClick={LikeMeAction} />
+    </>
+  );
+}
 
 export default App;
